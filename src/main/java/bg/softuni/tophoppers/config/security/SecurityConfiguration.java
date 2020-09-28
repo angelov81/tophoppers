f356488cbd439e7dbd49535257e8b20d47b17b67
Angel Angelov <Angel.Angelov@paysafe.com>
2020-09-28 19:18:22 +0300

    Add inMemoryAuthentication test

--- src/main/java/bg/softuni/tophoppers/config/security/SecurityConfiguration.java
+++ src/main/java/bg/softuni/tophoppers/config/security/SecurityConfiguration.java
@@ -1,32 +1,45 @@
 package bg.softuni.tophoppers.config.security;
 
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-  public final UserDetailsService userDetailsService;
+  final PasswordEncoder passwordEncoder;
 
-  public SecurityConfiguration(UserDetailsService userDetailsService) {
-    this.userDetailsService = userDetailsService;
+  public SecurityConfiguration(PasswordEncoder passwordEncoder) {
+    this.passwordEncoder = passwordEncoder;
+  }
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth
+        .inMemoryAuthentication()
+        .withUser("user")
+        .password(passwordEncoder.encode("pass")).roles("USER")
+        .and()
+        .withUser("admin")
+        .password(passwordEncoder.encode("admin")).roles("ADMIN", "USER");
   }
 
   @Override
   protected void configure(HttpSecurity httpSecurity) throws Exception {
     httpSecurity
         .authorizeRequests()
-//        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-        .antMatchers("/products**").permitAll();
+        .antMatchers("/index").permitAll()
+        .antMatchers("/categories.html").hasRole("ADMIN")
+        .antMatchers("/farms.html").hasRole("ADMIN")
+        .antMatchers("/products.html").hasRole("USER")
+        .and()
+        .formLogin();
 
     // Temp solution until login is implemented, TODO: delete me
-    httpSecurity.csrf().disable();
-    httpSecurity.headers().frameOptions().disable();
+//    httpSecurity.csrf().disable();
+//    httpSecurity.headers().frameOptions().disable();
 
   }
 
